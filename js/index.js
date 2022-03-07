4ae94e1b13dc91eebb1407efb91d0b149acac6ec
Sherlock67 <alammushfiqul366@gmail.com>
2022-03-07 20:16:44 +0600

    email validation

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const form = document.getElementById('input-area')
+const inputs = document.getElementsByTagName('input');
+
+
+
+function validateEmail (email) {
+	var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return re.test(String(email).toLowerCase());
+}
